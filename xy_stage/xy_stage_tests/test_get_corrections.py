ea7df5e5fe489a40f80f6470c80cc85d9e6fb9cc
David Igandan <david.igandan@diamond.ac.uk>
2024-12-12 17:47:54 +0000

    Add parametrized test

--- xy_stage/xy_stage_tests/test_get_corrections.py
+++ xy_stage/xy_stage_tests/test_get_corrections.py
@@ -4,9 +4,17 @@ from numpy.testing import assert_array_almost_equal
 from ..get_xy_corrections import get_corrections
 import pytest
 
-def test_get_corrections():
-        result = get_corrections(30,5)
-        expect = np.array([[5.0000000e+00],[4.1948373e-17]])
-        assert_array_almost_equal(result, expect, decimal=4)
+# def test_get_corrections():
+#         result = get_corrections(30,5)
+#         expect = np.array([[5.0000000e+00],[4.1948373e-17]])
+#         assert_array_almost_equal(result, expect, decimal=7)
     
+    
+@pytest.mark.parametrize("angle, error_magnitude, expected", [
+    (30, 5,np.array([[5.0000000e+00],[4.1948373e-17]]) ),
+])
+
+def test_get_corrections(angle, error_magnitude, expected):
+    result = get_corrections(angle, error_magnitude)
+    assert_array_almost_equal(result, expected, decimal=7)
     
