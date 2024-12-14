a2492facc581cc24487070e5e25edaf0a36b4ddd
David Igandan <david.igandan@diamond.ac.uk>
2024-12-14 19:06:41 +0000

    minor changes

--- xy_stage/xy_stage_tests/test_get_corrections.py
+++ xy_stage/xy_stage_tests/test_get_corrections.py
@@ -4,11 +4,14 @@ from numpy.testing import assert_array_almost_equal
 from ..get_xy_corrections import get_corrections
 import pytest
 
-@pytest.mark.parametrize("angle, error_magnitude, expected", [
-    (30, 5,np.array([[5.0000000e+00],[4.1948373e-17]]) ),
-])
 
+@pytest.mark.parametrize(
+    "angle, error_magnitude, expected",
+    [
+        (30, 5, np.array([[5.0000000e00], [4.1948373e-17]])),
+    ],
+)
 def test_get_corrections(angle, error_magnitude, expected):
     result = get_corrections(angle, error_magnitude)
+
     assert_array_almost_equal(result, expected, decimal=7)
-    
